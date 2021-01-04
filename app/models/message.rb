1f5f3df3fb92fbc1f0baf88070d7b4c5edd6ced4
Steven Chang <stevenchang@osp.com.tw>
2021-01-04 19:14:38 +0800

    FIXME: after_destroy_commit -> boradcast_remove_to does not work in Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   scope :desc_order, -> { order(created_at: :desc)  }
   after_create_commit -> { broadcast_append_to room }
+
+  # FIXME: this line does not work
+  after_destroy_commit -> { broadcast_remove_to room }
 end
