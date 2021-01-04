fff5e13099d407717ef0b2fb4eaf91a9b504aedc
Steven Chang <stevenchang@osp.com.tw>
2021-01-04 18:02:03 +0800

    Add Stimulus controller to reset form, add Action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room
+  scope :desc_order, -> { order(created_at: :desc)  }
+  after_create_commit -> { broadcast_append_to room }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   has_many :messages
+  scope :desc_order, -> { order(id: :desc) }
 end
