598beb30469b9800d97911af7c2377f7096e5df3
Steven Chang <stevenchang@osp.com.tw>
2021-01-05 11:26:07 +0800

    Update broadcast callback in message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :room
   scope :desc_order, -> { order(created_at: :desc)  }
-  after_create_commit -> { broadcast_append_to room }
 
-  # FIXME: this line does not work
-  after_destroy_commit -> { broadcast_remove_to room }
+  broadcasts_to :room, inserts_by: :prepend
+  # replace below 3 callbacks with broadcasts_to, a full menu of basic lifecycle updates
+  # after_create_commit -> { broadcast_prepend_to room }
+  # after_destroy_commit -> { broadcast_remove_to room }
+  # after_update_commit -> { broadcast_replace_to room }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
   scope :desc_order, -> { order(id: :desc) }
 end
